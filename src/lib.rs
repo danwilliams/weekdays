ed7273e0af308414fff4002fbe7010c0bf864c5d
Dan Williams <dan@dotfive.co.uk>
2024-11-02 04:07:26 +0000

    Added standard linting configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
 //! The Weekdays crate provides days of the week bit-mapped in a single byte.
 
 
+
+//		Global configuration
+
+//	Customisations of the standard linting configuration
+#![allow(clippy::items_after_test_module, reason = "Not needed with separated tests")]
+
+//	Lints specifically disabled for unit tests
+#![cfg_attr(test, allow(
+	non_snake_case,
+	clippy::arithmetic_side_effects,
+	clippy::cast_lossless,
+	clippy::cast_precision_loss,
+	clippy::cognitive_complexity,
+	clippy::default_numeric_fallback,
+	clippy::exhaustive_enums,
+	clippy::exhaustive_structs,
+	clippy::expect_used,
+	clippy::indexing_slicing,
+	clippy::let_underscore_must_use,
+	clippy::let_underscore_untyped,
+	clippy::missing_assert_message,
+	clippy::missing_panics_doc,
+	clippy::must_use_candidate,
+	clippy::panic,
+	clippy::print_stdout,
+	clippy::too_many_lines,
+	clippy::unwrap_in_result,
+	clippy::unwrap_used,
+	reason = "Not useful in unit tests"
+))]
+
+
